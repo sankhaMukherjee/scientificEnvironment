7c5de87bafc9bd0be5b7ef2c42c6083b429fb21e
Sankha Subhra Mukherjee <sankha.mukherjee@gmail.com>
2015-11-01 20:38:53 +0800

    adding the sublime text preference file

---
+++ sublimeTextPreferences.py
@@ -0,0 +1,9 @@
+{
+"caret_style": "phase",
+"color_scheme": "Packages/Color Scheme - Default/Solarized (Dark).tmTheme",
+"font_size": 12,
+"highlight_line": true,
+"show_minimap": false,
+"tab_size": 4,
+"translate_tabs_to_spaces": true
+}
